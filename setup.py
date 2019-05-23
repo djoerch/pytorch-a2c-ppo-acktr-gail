e227186c48374e699eb758965e5d9010666f22e7
Daniel Jorgens <daniel.jorgens@sth.kth.se>
2019-05-23 13:32:28 +0200

    ADM: installation and execution script.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+import glob
+
 from setuptools import find_packages, setup
 
 setup(
     name='a2c-ppo-acktr',
     packages=find_packages(),
     version='0.0.1',
-    install_requires=['gym', 'matplotlib', 'pybullet'])
+    install_requires=['gym', 'matplotlib', 'pybullet'],
+    scripts=glob.glob('scripts/*')
+)
